82c12fa950905c069a8bba8662cfc05e72c1a14d
Anton Filippov <derevenchshina@yandex.ru>
2022-07-26 18:29:01 +0500

    add map controller add buttons to map controller add segues for map controller

---
+++ MyPlacesApp/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  MyPlacesApp
+//
+//  Created by Антон Филиппов on 26.07.2022.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeView() {
+        dismiss(animated: true)
+    }
+    
+}
