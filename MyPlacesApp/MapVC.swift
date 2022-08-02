a23315c570ac8af77ce4aa2019d03c78c1ca346a
Anton Filippov <derevenchshina@yandex.ru>
2022-08-02 16:25:51 +0500

    add user location

--- MyPlacesApp/MapVC.swift
+++ MyPlacesApp/MapVC.swift
@@ -7,19 +7,24 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
-
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
-    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         setUpPlaceMark()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        checkLocationServices()
     }
     
     @IBAction func closeView() {
@@ -51,6 +56,49 @@ class MapVC: UIViewController {
         
     }
     
+    private func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setUpLocalitonManager()
+            checkLocationAuthorization()
+        } else {
+            
+        }
+    }
+    
+    private func setUpLocalitonManager() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+    }
+    
+    private func checkLocationAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        case .denied:
+            presentAlertController(title: "Ooops", message: "Let your phone to watch you!")
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //Show allert controller
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print ("New case is avaliable")
+        }
+        
+    }
+    
+    private func presentAlertController(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
 
@@ -71,5 +119,11 @@ extension MapVC: MKMapViewDelegate {
             annotationView?.rightCalloutAccessoryView = imageView
         }
         return annotationView
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
     }
 }
